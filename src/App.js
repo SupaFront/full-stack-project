5026ada4e29278d469826a19c1918844d13e0538
Istomina Alena <mynikisalena@gmail.com>
2022-05-04 22:29:06 +0300

    Create layout for header and mobile menu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './modules/Header/Header';
 import MyRoutes from './Routes';
 
 function App() {
 	return (
 		<div className="App">
+			<Header />
 			<MyRoutes />
 		</div>
 	);
