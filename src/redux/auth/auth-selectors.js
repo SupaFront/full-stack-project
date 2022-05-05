eb245d19e239f3a9f530fe9d39996267dad2d225
SupaFront <bluewotka@gmail.com>
2022-05-05 15:40:48 +0300

    redux is mostly done

--- src/redux/auth/auth-selectors.js
+++ src/redux/auth/auth-selectors.js
@@ -1,7 +1,6 @@
 export const getIsLoggedIn = state => state.auth.isLoggedIn;
 export const getUserId = state => state.auth.user._id;
-export const getIsLoading = state => state.auth.isLoading;
+export const getisLoading = state => state.auth.loading;
 export const getToken = state => state.auth.token;
 export const getUserEmail = state => state.auth.user.email;
 export const getError = state => state.auth.error;
-export const getMessage = state => state.auth.message;
--- src/redux/qa-tests/qa-test-selectors.js
+++ src/redux/qa-tests/qa-test-selectors.js
@@ -1,1 +1,4 @@
 export const getQuestions = state => state.qaTests.questions;
+export const getisLoading = state => state.auth.loading;
+export const getResults = state => state.qaTests.result;
+export const getError = state => state.auth.error;
