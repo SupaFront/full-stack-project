e0b5640a94daafe8bf0695766d06bbfc587ed21e
SupaFront <bluewotka@gmail.com>
2022-05-05 14:09:35 +0300

    api is done

--- src/API/auth.js
+++ src/API/auth.js
@@ -1,5 +1,49 @@
 import axios from 'axios';
 
+axios.defaults.baseURL = 'http://localhost:3000/api/';
+const path = {
+  register: 'auth/register',
+  login: 'auth/login',
+  logout: 'auth/logout',
+  current: 'auth/current',
+};
+
 export const setToken = token => (axios.defaults.headers.common.Authorization = `Bearer ${token}`);
 
 export const unsetToken = () => (axios.defaults.headers.common.Authorization = null);
+
+export const loginUser = async user => {
+  try {
+    const { data } = await axios.post(path.login, user);
+    return data;
+  } catch (error) {
+    throw error.message;
+  }
+};
+
+export const registerUser = async user => {
+  try {
+    const { data } = await axios.post(path.register, user);
+    return data;
+  } catch (error) {
+    throw error.message;
+  }
+};
+
+export const getCurrentUser = async () => {
+  try {
+    const { data } = await axios.get(path.current);
+    return data;
+  } catch (error) {
+    throw error.message;
+  }
+};
+
+export const logoutUser = async () => {
+  try {
+    await axios.post(path.logout);
+    return 'success';
+  } catch (err) {
+    throw err.message;
+  }
+};
--- src/API/qa-test.js
+++ src/API/qa-test.js
@@ -1,1 +1,24 @@
 import axios from 'axios';
+
+const path = {
+  tests: 'tests/',
+  results: 'tests/results/',
+};
+
+export const getQuestions = async questionType => {
+  try {
+    const { data } = axios.get(path.tests + questionType);
+    return data;
+  } catch (error) {
+    throw error.message;
+  }
+};
+
+export const getResults = async (questionType, answers) => {
+  try {
+    const { data } = axios.post(path.results + questionType, answers);
+    return data;
+  } catch (error) {
+    throw error.message;
+  }
+};
