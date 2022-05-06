1bbbd75e517e6422e82c3ecf80851a6439344869
Volodymyr Riabushko <riabushko@gmail.com>
2022-05-06 17:38:29 +0300

    next

--- src/shared/components/QuestForm/QuestForm.jsx
+++ src/shared/components/QuestForm/QuestForm.jsx
@@ -1,8 +1,14 @@
 import QuestList from "../QuestList";
+import GetSvg from "../GetSvg";
+import Radiobuttons from "../Radiobuttons/Radiobuttons";
 
 import s from "./questForm.module.css";
 
-const QuestForm = ({ questCurrent = 1, questCount = 12, questText="What is regression testing?" }) => {
+const QuestForm = ({
+  questCurrent = 1,
+  questCount = 12,
+  questText = "What is regression testing?",
+}) => {
   return (
     <div>
       <form className={s.form} action="#">
@@ -10,9 +16,10 @@ const QuestForm = ({ questCurrent = 1, questCount = 12, questText="What is regre
           Question <span>{questCurrent}</span> / {questCount}
         </h2>
         <p className={s.questText}>{questText}</p>
-        <QuestList list={[]} />;
+        <QuestList list={[]} />
+        <Radiobuttons />
       </form>
-     
+      
     </div>
   );
 };
