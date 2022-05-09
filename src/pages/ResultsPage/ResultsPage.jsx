c8c0dbae928807da87adce078dcd1f79c4ba86e0
SupaFront <bluewotka@gmail.com>
2022-05-09 15:42:19 +0300

    results page needs fixes

--- src/pages/ResultsPage/ResultsPage.jsx
+++ src/pages/ResultsPage/ResultsPage.jsx
@@ -3,32 +3,35 @@ import { Link } from 'react-router-dom';
 import Diagram from '../../modules/Diagram/';
 import { clearQuestionsList } from '../../redux/qa-tests/qa-test-slice';
 import styles from './ResultsPage.module.css';
+import catImage from '../../images/cat.png';
 
 function ResultsPage() {
   const dispatch = useDispatch();
 
   return (
-    <main>
+    <div className={styles.container}>
       <h1 className={styles.headline}>Results</h1>
-      <h2 className={styles.title}>{'[ testing theory_]'}</h2>
+      <h2 className={styles.title}>{'[ TESTING THEORY_]'}</h2>
       <Diagram />
       <div>
         <ul>
           <li>
-            <p className={styles.resultsStats}></p>
+            <p className={styles.stats}>Correct</p>
           </li>
           <li>
-            <p className={styles.resultsStats}></p>
+            <p className={styles.stats}>Incorrect</p>
           </li>
         </ul>
       </div>
       <div>
-        <img className={styles.catPic} alt={'a cat'} />
-        <p className={styles.mainMessage}></p>
-        <p className={styles.secondaryMessage}></p>
-        <Link to="/test" onClick={() => dispatch(clearQuestionsList())} />
+        <img src={catImage} className={styles.catpic} alt={'a cat'} />
+        <p className={styles.mainMessage}>Bad!</p>
+        <p className={styles.secondaryMessage}>You're the worst motherfucker QA!</p>
+        <Link to="/test" onClick={() => dispatch(clearQuestionsList())}>
+          Try again
+        </Link>
       </div>
-    </main>
+    </div>
   );
 }
 
