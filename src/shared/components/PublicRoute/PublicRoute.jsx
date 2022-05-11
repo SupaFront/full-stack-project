a25e40473060d3815daf7c43ae22212ffeb6c894
Volodymyr Riabushko <riabushko@gmail.com>
2022-05-11 13:29:29 +0300

    Update PublicRoute.jsx

--- src/shared/components/PublicRoute/PublicRoute.jsx
+++ src/shared/components/PublicRoute/PublicRoute.jsx
@@ -1,9 +1,11 @@
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from "react-router-dom";
 
-import useAuth from '../../hooks/useAuth';
+import useAuth from "../../hooks/useAuth";
 
 const PublicRoute = () => {
   const isLoggedIn = useAuth();
+  
+  isLoggedIn = true;//only for tests
 
   if (isLoggedIn) {
     return <Navigate to="/" />;
