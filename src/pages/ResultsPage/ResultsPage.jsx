ca0d105628a28d92a07e33759999046d6a0fc7f4
Istomina Alena <mynikisalena@gmail.com>
2022-05-12 18:08:00 +0300

    add decorative lines

--- src/pages/ResultsPage/ResultsPage.jsx
+++ src/pages/ResultsPage/ResultsPage.jsx
@@ -6,37 +6,39 @@ import styles from './ResultsPage.module.css';
 import catImage from '../../images/cat.png';
 
 function ResultsPage() {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  return (
-    <main>
-      <div className={styles.container}>
-        <h1 className={styles.headline}>Results</h1>
-        <h2 className={styles.title}>{'[ TESTING THEORY_]'}</h2>
-        <Diagram />
-        <div>
-          <div className={styles.statscontainer}>
-            <span className={styles.stats}>Correct answers - 9</span>
-            <span className={styles.stats}>Total questions - 12</span>
-          </div>
-        </div>
-        <div className={styles.lowestsect}>
-          <img src={catImage} className={styles.catpic} alt={'a cat'} />
-          <p className={styles.mainmessage}>Not Bad!</p>
-          <p className={styles.secondarymessage}>But you still need to learn some materials.</p>
-          <Link
-            className={styles.btn}
-            to="/test"
-            onClick={() => {
-              // dispatch(clearQuestionsList())
-            }}
-          >
-            Try again
-          </Link>
-        </div>
-      </div>
-    </main>
-  );
+	return (
+		<main>
+			<div className={ styles.container }>
+				<h1 className={ styles.headline }>Results</h1>
+				<div className={ styles.titleContainer }>
+					<h2 className={ styles.title }>{ '[ TESTING THEORY_]' }</h2>
+				</div>
+				<Diagram />
+				<div>
+					<div className={ styles.statscontainer }>
+						<span className={ styles.stats }>Correct answers - 9</span>
+						<span className={ styles.stats }>Total questions - 12</span>
+					</div>
+				</div>
+				<div className={ styles.lowestsect }>
+					<img src={ catImage } className={ styles.catpic } alt={ 'a cat' } />
+					<p className={ styles.mainmessage }>Not Bad!</p>
+					<p className={ styles.secondarymessage }>But you still need to learn some materials.</p>
+					<Link
+						className={ styles.btn }
+						to="/test"
+						onClick={ () => {
+							// dispatch(clearQuestionsList())
+						} }
+					>
+						Try again
+					</Link>
+				</div>
+			</div>
+		</main>
+	);
 }
 
 export default ResultsPage;
